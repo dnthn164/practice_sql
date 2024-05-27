3f82ad5c1cd0fed13d842071022303de987ad61f
Thiên Ân <dn.thn164@gmail.com>
2024-05-27 17:23:15 +0700

    Create practice_01.sql

---
+++ practice_01.sql
@@ -0,0 +1,29 @@
+-- 1. https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+
+--2. https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+
+--3. https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+
+--4. https://www.hackerrank.com/challenges/weather-observation-station-6/problem?isFullScreen=true
+
+--5. https://www.hackerrank.com/challenges/weather-observation-station-7/problem?isFullScreen=true
+
+--6. https://www.hackerrank.com/challenges/weather-observation-station-9/problem?isFullScreen=true
+
+--7. https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+
+--8. https://www.hackerrank.com/challenges/salary-of-employees/problem?isFullScreen=true
+
+--9. https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50
+
+--10. https://leetcode.com/problems/big-countries/description/?envType=study-plan-v2&envId=top-sql-50
+
+--11. https://leetcode.com/problems/big-countries/description/?envType=study-plan-v2&envId=top-sql-50
+
+--12. https://leetcode.com/problems/article-views-i/description/?envType=study-plan-v2&envId=top-sql-50
+
+--13. https://datalemur.com/questions/tesla-unfinished-parts
+
+--14. https://platform.stratascratch.com/coding/10003-lyft-driver-wages?code_type=1
+
+--15. https://platform.stratascratch.com/coding/10002-find-the-advertising-channel-where-uber-spent-more-than-100k-usd-in-2019?code_type=1
