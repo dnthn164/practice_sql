077f101de3df4d6a845cb9991b195135bfec4ea0
Thiên Ân <dn.thn164@gmail.com>
2024-05-31 15:44:53 +0700

    Create practice_02.sql

---
+++ practice_02.sql
@@ -0,0 +1,23 @@
+--1. https://www.hackerrank.com/challenges/weather-observation-station-3/problem?isFullScreen=true
+
+--2. https://www.hackerrank.com/challenges/weather-observation-station-4/problem?isFullScreen=true
+
+--3. https://www.hackerrank.com/challenges/the-blunder/problem?isFullScreen=true
+
+--4. https://datalemur.com/questions/alibaba-compressed-mean
+
+--5. https://datalemur.com/questions/matching-skills
+
+--6. https://datalemur.com/questions/sql-average-post-hiatus-1
+
+--7. https://datalemur.com/questions/cards-issued-difference
+
+--8. https://datalemur.com/questions/non-profitable-drugs
+
+--9. https://leetcode.com/problems/not-boring-movies/?envType=study-plan-v2&envId=top-sql-50
+
+--10. https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/description/?envType=study-plan-v2&envId=top-sql-50
+
+--11. https://leetcode.com/problems/find-followers-count/description/?envType=study-plan-v2&envId=top-sql-50
+
+--12. https://leetcode.com/problems/classes-more-than-5-students/description/?envType=study-plan-v2&envId=top-sql-50
